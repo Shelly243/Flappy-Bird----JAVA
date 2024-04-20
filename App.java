1e26c64c585c08659397c5a722af89ffa9a86446
Diya Lohani <shellylohani2018@gmail.com>
2024-04-20 21:12:00 +0530

    update FlappyBird

--- App.java
+++ App.java
@@ -6,8 +6,8 @@ public class App {
         int boardHeight = 640;
 
         JFrame frame = new JFrame("Flappy Bird");
-        // frame.setVisible(true);
-		frame.setSize(boardWidth, boardHeight);
+
+        frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
